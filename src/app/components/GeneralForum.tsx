bf584bb509329c87970b25446dde722b61191ab8
AdrianKnudsen <adrian@knudsen.no>
2024-07-01 13:29:22 +0200

    bug fix

--- src/app/components/GeneralForum.tsx
+++ src/app/components/GeneralForum.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ForumSection from "./ForumSection";
 
-export default function GeneralForum() {
+export default function generalForum() {
   return <ForumSection category="generalPost" title="General" />;
 }
--- src/app/components/TechnologyForum.tsx
+++ src/app/components/TechnologyForum.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ForumSection from "./ForumSection";
 
-export default function TechnologyForum() {
+export default function technologyForum() {
   return <ForumSection category="technologyPost" title="Technology" />;
 }
